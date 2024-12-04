02af80e213012d58fc7cbce912824fbf3809e093
michalcapoun <michal.capoun@gmail.com>
2024-12-04 09:41:29 +0100

    add maven dependency

---
+++ src/main/java/com/michalcapoun/maparchive/MapArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.michalcapoun.maparchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapArchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michalcapoun/maparchive/MapArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalcapoun.maparchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
